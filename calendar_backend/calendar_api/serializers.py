763d17387b0d8179ad73e20abb2192dc3ab9a40e
Isham Bhandar <ishambhandari007@gmail.com>
2024-06-06 01:36:05 +0545

    Fix: fixed overlapping issue

--- calendar_backend/calendar_api/serializers.py
+++ calendar_backend/calendar_api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Events
+
+from django.db.models import Q
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
@@ -20,14 +22,17 @@ class EventsSerializer(serializers.ModelSerializer):
         if end_time and start_time and end_time <= start_time:
             raise serializers.ValidationError("end_time must be after start_time")
 
-
         if start_time and end_time:
+            # Filter overlapping events for the same user
+            user_id = self.context['request'].user.id
             overlapping_events = Events.objects.filter(
-                start_time__lt=end_time,
-                end_time__gt=start_time
+                Q(start_time__lt=end_time, end_time__gt=start_time) &
+                (Q(created_by=user_id) | Q(created_by__isnull=True))
             )
-            if self.instance:  
+            
+            if self.instance:
                 overlapping_events = overlapping_events.exclude(id=self.instance.id)
+                
             if overlapping_events.exists():
                 raise serializers.ValidationError("The event overlaps with existing events.")
 
